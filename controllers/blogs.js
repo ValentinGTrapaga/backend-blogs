4a306d084f2c03f9535e897b23b3ba6ee43f465e
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-11-02 06:37:14 -0300

    add put and delete functionality

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,12 +7,45 @@ blogRouter.get('/', (request, response) => {
   })
 })
 
+blogRouter.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  Blog.findById(id)
+    .then((blog) => {
+      blog ? res.json(blog) : res.status(404).end()
+    })
+    .catch((err) => next(err))
+})
+
+blogRouter.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  Blog.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
 blogRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
 
   blog.save().then((result) => {
     response.status(201).json(result)
   })
+})
+
+blogRouter.put('/:id', (req, res, next) => {
+  const body = req.body
+  const id = req.params.id
+  const blogToAdd = { ...body, id }
+  Blog.findByIdAndUpdate(id, blogToAdd, {
+    new: true,
+    runValidators: true,
+    context: 'query'
+  })
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((err) => next(err))
 })
 
 module.exports = blogRouter
